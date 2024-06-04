f5e3fe89b64fbf8755cd51efe23ea9a6d99d9441
PradhumnAgarwal <pradhumnagarwal94@gmail.com>
2024-06-04 22:47:57 +0530

    question endpoint

--- index.js
+++ index.js
@@ -17,6 +17,41 @@ app.get("/", (req, res)=>{
     res.send(`Server is running on PORT:${PORT}.`);
 })
 
+app.get("/ques/:id", async(req, res)=>{
+    const id = req.params.id;
+    let query= `query Problems {
+        problem(where: {prob_id: ${id}}) {
+          prob_id
+          prob_title
+          difficulty
+          problemStatement
+          inputFormat
+          outputFormat
+          constraints
+          testcases {
+            tc_id
+            tc_input
+            tc_output
+            tc_explanation
+            image
+            isHidden
+          }
+        }
+      }
+       `
+  
+    let response = await fetch(`https://api-ap-south-1.hygraph.com/v2/clx0g9rkn04qv07w8t1mdf12x/master?query=${query}`, { 
+    method: "GET",
+});
+
+    const data = await response.json();
+    if(!data.data.problem) {
+        res.status(404).send("Problem not found")
+        return;
+    }
+    res.send(data.data.problem);
+})
+
 io.on('connection', (socket)=>{
     socket.emit('me', socket.id);
 
