df29364d319e943962e42377b9a0487d23d99b05
PradhumnAgarwal <pradhumnagarwal94@gmail.com>
2024-06-07 14:47:41 +0530

    fight page remove

--- client/src/app/fight/page.tsx
+++ client/src/app/fight/page.tsx
@@ -1,11 +1,12 @@
-import Workspace from "./_components/Workspace";
-import { twoSum } from "@/utils/problems/twoSum";
+// import Workspace from "./_components/Workspace";
+// import { twoSum } from "@/utils/problems/twoSum";
 
 export default function Fight() {
   return (
     <div className="h-full w-full flex" style={{ minHeight: "100vh" }}>
       <div className="w-[80%]">
-        <Workspace problem={twoSum} />
+        {/* <Workspace problem={twoSum} /> */}
+        This page is down
       </div>
       <div className="w-[20%] bg-gray-800 text-white"></div>
     </div>
