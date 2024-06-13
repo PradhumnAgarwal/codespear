e4b889c2ba6f66526b8223dfaf8e53d2ecd11986
PradhumnAgarwal <pradhumnagarwal94@gmail.com>
2024-06-13 19:40:12 +0530

    sdlfk

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,18 @@
-const path = require('path');
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import withTM from 'next-transpile-modules';
 
-module.exports = {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withTM([])({
   experimental: {
     appDir: true,
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias['@'] = join(__dirname, 'src');
     return config;
   },
-};
+});
+
+export default nextConfig;
