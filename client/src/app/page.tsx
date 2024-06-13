779e3ecca96715f8312c78353fec72cb97eb5e9f
PradhumnAgarwal <pradhumnagarwal94@gmail.com>
2024-06-13 19:41:46 +0530

    sdlfk

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,12 @@
-import LandingHero  from "../components/LandingHero";
-import LandingNavbar  from "../components/LandingNav";
+// import LandingHero  from "../components/LandingHero";
+// import LandingNavbar  from "../components/LandingNav";
 
 export default function Home() {
   return (
     <div className=" h-full max-w-screen-xl mx-auto" style={{minHeight: '100vh'}}>
-      <LandingNavbar />
-      <LandingHero />
+      {/* <LandingNavbar />
+      <LandingHero /> */}
+      <div className="h-full w-full flex" style={{ minHeight: "100vh" }}>jfgnflkmlgkmfllg</div>
     </div>
   );
 }
