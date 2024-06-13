5a67cf83b0216971db23c196e856f28d070fbbda
PradhumnAgarwal <pradhumnagarwal94@gmail.com>
2024-06-13 19:51:25 +0530

    sdlfk

--- client/src/components/landingHero.tsx
+++ client/src/components/landingHero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 // import TypewriterComponent from "typewriter-effect";
+import Typewriter from "typewriter-effect";
 import Link from "next/link";
 
 const LandingHero = () => {
@@ -11,19 +12,13 @@ const LandingHero = () => {
         <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
           <h1>The Best AI Tool for</h1>
           <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-            {/* <TypewriterComponent
+            <Typewriter
               options={{
-                strings: [
-                  "Chatbot.",
-                  "Photo Generation.",
-                  "Music Generation.",
-                  "video Generation.",
-                  "Code Generation.",
-                ],
+                strings: ["Hello", "World", "NewCheck"],
                 autoStart: true,
                 loop: true,
               }}
-            /> */}
+            />
           </div>
         </div>
         <div className="text-sm md:text-xl font-light text-zinc-400">
@@ -41,7 +36,7 @@ const LandingHero = () => {
         </div>
 
         <div className="gradient-02 z-0" />
-        
+
         <div className="text-zinc-400 text-xs md:text-sm font-normal">
           No credit card required.
         </div>
@@ -50,4 +45,4 @@ const LandingHero = () => {
   );
 };
 
-export default LandingHero
+export default LandingHero;
